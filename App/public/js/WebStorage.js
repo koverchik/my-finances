91e9f1020463ab4427344c2617681f295125ffaa
koverchik <koverchik-work@yandex.ru>
2020-04-16 21:00:35 +0300

    add: adding rows to a table

---
+++ App/public/js/WebStorage.js
@@ -0,0 +1,79 @@
+document.addEventListener("DOMContentLoaded", ready);
+function ready() {
+
+let valueArray = new Array();
+
+$("#button_Add").click(function() {
+  let valueSizeCost = document.getElementById('size_cost').value;
+  let check = parseFloat(valueSizeCost);
+    if (check){
+
+  }else{
+
+  }
+
+//Копируются данные value в локальное хранилище storage.setItem(названиеКлюча, значениеКлюча);
+
+//Создание массива значений
+let keyCost = "keyCost" + Date.now();
+let valueNameCost = document.getElementById('name_cost').value;
+
+let costObj = {
+  "valueName": valueNameCost,
+  "valueSize": valueSizeCost
+};
+
+localStorage.setItem(keyCost, JSON.stringify(costObj));
+
+let localvalueArray = valueArray.push(keyCost);
+
+
+
+
+//Вставляется строка со значениями из локального хранилица
+//Добавление ячеек создания элементов
+function createOutlayPar(keyCost) {
+  //Получение значения одной записи по ключу
+    let localArray = localStorage.getItem(keyCost);
+    let value = JSON.parse(localArray);
+    //Создание строки таблциы
+    let tr = document.createElement('tr');
+    let tbody = document.createElement('tbody');
+    tbody.append(tr);
+    let namberRow = valueArray.length;
+    let tdNamberRow = document.createElement('td');
+    tdNamberRow.innerHTML = namberRow;
+    tr.append(tdNamberRow);
+
+    let tdValueName = document.createElement('td');
+    tdValueName.innerHTML = value.valueName;
+    tr.append(tdValueName);
+
+    let tdValueSizeSize = document.createElement('td');
+    tdValueSizeSize.innerHTML = value.valueSize;
+    tr.append(tdValueSizeSize);
+    $("#table").append(tbody);
+    //Удаление значений value из дом документа
+    $("#name_cost").val('');
+    $("#size_cost").val('');
+
+}
+createOutlayPar(keyCost);
+
+//Выводит сумму
+function sumAllValues() {
+  let sumValues = 0;
+  for (let i = 0; i < valueArray.length; i++) { // выведет 0, затем 1, затем 2
+    let sumLocal = localStorage.getItem(valueArray[i]);
+    let valueOne = JSON.parse(sumLocal);
+    sumValues = valueOne.valueSize + sumValues;
+  }
+  return sumValues;
+}
+
+sumValues = sumAllValues();
+$("#sumAValues").replaceWith($("#sumAValues").text(sumValues));
+}
+);
+
+}
--- App/resources/views/auth/table/outlay.blade.php
+++ App/resources/views/auth/table/outlay.blade.php
@@ -3,30 +3,32 @@
 @section('content')
   <div class="container">
       <h2>Смета</h2>
-      <table class="table table-sm caption">
-      <thead class="thead-dark">
-        <tr>
-          <th>Дата</th>
-          <th>Название</th>
-          <th>Сумма</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>data</td>
-          <td>data</td>
-          <td>data</td>
-        </tr>
-      </tbody>
-      <tbody>
+      <table id="table" class="table table-sm caption">
+        <thead class="thead-dark">
+          <tr>
+            <th>#</th>
+            <th>Название</th>
+            <th>Сумма</th>
+          </tr>
+        </thead>
+    </table>
+    <table class="table table-sm caption">
+      <thead>
         <tr>
           <td colspan="2" class="text-right">Итоговая сумма:</td>
-          <td class="bg-info">Сумма</td>
+          <td id="sumAValues" class="bg-info">0</td>
         </tr>
-      </tbody>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td><input id="name_cost" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" placeholder="Наименование"></td>
+            <td><input  id="size_cost"type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" placeholder="2.34"></td>
+          </tr>
+        </tbody>
     </table>
     <div class="d-flex justify-content-end">
-      <button class="btn btn-info rounded-0">+ Добавить</button>
+      <button id="button_Add" class="btn btn-info rounded-0">+ Добавить</button>
     </div>
   </div>
 @endsection
