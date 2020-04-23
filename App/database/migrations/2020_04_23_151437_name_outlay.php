d21052de7ae468c7c6897e2fd38ef5c9176fd929
koverchik <koverchik-work@yandex.ru>
2020-04-23 20:00:54 +0300

    add: migrations for outlay

---
+++ App/database/migrations/2020_04_23_151437_name_outlay.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class NameOutlay extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('name_outlay', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('name_outlay');
+    }
+}
---
+++ App/database/migrations/2020_04_23_161048_row_outlay.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RowOutlay extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rows_outlay', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 150);
+            $table->float('amount', 8, 2);
+            $table->bigInteger('user_id')->unsigned();
+            $table->integer('name_outlay_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('name_outlay_id')->references('id')->on('name_outlay')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rows_outlay');
+    }
+}
